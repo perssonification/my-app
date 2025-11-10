7498881406535552d34dd9c2e4bb06c0c98cdbf0
perssonification <krispinpers@gmail.com>
2025-11-10 16:22:14 +0200

    out putting lists and props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,19 @@
+import { useState } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
-
-  const handleClick = () => {
-    console.log('you been clicked');
-  }
-
-  const handleClickAgain = (name) => {
-    console.log('hello ' + name);
-  }
-
+  const [blogs, setBlogs] = useState ([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 }   
+  ]);
+  
   return (
     <div className="home">
-      <h2>Home Page</h2>
-      <button onClick ={handleClick}>Click Me</button>
-      <button onClick ={() => handleClickAgain('mario')}>Click me again</button>
+      <BlogList blogs={blogs} title= "All Blogs!"/>
+
     </div>
   );
-};
+}
 
 export default Home;
